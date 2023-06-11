04feaae894c72952bb97766590da16d5a155fe4d
Mahdi Roohi <mail@mroohi.ir>
2023-06-11 22:27:11 +0330

    change PiecesGroup class and connect to Piece

--- src/main/java/com/chess/base/PiecesGroup.java
+++ src/main/java/com/chess/base/PiecesGroup.java
@@ -1,15 +1,24 @@
 package com.chess.base;
 
+import java.util.ArrayList;
+
 public class PiecesGroup {
     public Objects.PiecesColor Color;
-    // private ArrayList<piece> Pieces;
-
-
+    private ArrayList<Piece> Pieces;
     public Objects.PiecesColor getColor() {
         return Color;
     }
-
     public void setColor(Objects.PiecesColor color) {
         Color = color;
+    }
+    public boolean AddPieceToGroup(Piece pic) {
+       return Pieces.add(pic);
+    }
+    public boolean RemovePieceFromGroup(Piece pic) {
+        return Pieces.remove(pic);
+    }
+
+    public ArrayList<Piece> getPieces() {
+        return Pieces;
     }
 }
