975d81e66ff4895a4cef137e4fd3f6902b593545
Mahdi Roohi <mail@mroohi.ir>
2023-06-11 21:34:24 +0330

    create objects class and game class

---
+++ src/main/java/com/chess/base/Game.java
@@ -0,0 +1,17 @@
+package com.chess.base;
+
+public class Game {
+    //Board mainBoard;
+    //PiecesGroup WhitePieces;
+    //PiecesGroup BlackPieces;
+
+    /*
+    public void DefaultSetup() {
+
+    }
+    public void CustomSetup() {
+    
+    }
+
+    */
+}
---
+++ src/main/java/com/chess/base/Objects.java
@@ -0,0 +1,12 @@
+package com.chess.base;
+
+public class Objects {
+    public static enum PiecesColor {
+        White,
+        Black
+    }
+    public static enum HousesColor {
+        White,
+        Black
+    }
+}
