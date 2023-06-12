d6c0c3f492e9b384056457f42c2ae370e11114c7
Mahdi Roohi <mail@mroohi.ir>
2023-06-13 01:32:14 +0330

    complete AvailableMoves for Rook

--- src/main/java/com/chess/base/Rook.java
+++ src/main/java/com/chess/base/Rook.java
@@ -2,12 +2,33 @@ package com.chess.base;
 
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class Rook extends Piece {
     public Rook(PiecesGroup group, Game gameObj) {
         super(Objects.ChessPieces.Rook, group, gameObj, 5);
     }
     public ArrayList<Objects.Move> AvailableMoves() {
-        // toDo
-        return null;
+        ArrayList<Objects.Move> availableMoves = new ArrayList<>();
+
+        for (int i = -this.getPiecePosition().getX(); i < 8 - this.getPiecePosition().getX(); i++) {
+            boolean att = false;
+            Objects.Position newPosition = new Objects.Position(this.getPiecePosition().getX() + i, this.getPiecePosition().getY());
+            if (!java.util.Objects.isNull(this.getGameObj().getPieceByPosition(newPosition)) && this.getGameObj().getPieceByPosition(newPosition).getGroup() != this.getGroup())
+                att = true;
+            Objects.Move move = new Objects.Move(this, this.getPiecePosition(), newPosition, att);
+            if (this.getGameObj().CheckMove(move))
+                availableMoves.add(move);
+        }
+        for (int j = -this.getPiecePosition().getY(); j < 8 - this.getPiecePosition().getY(); j++) {
+            boolean att = false;
+            Objects.Position newPosition = new Objects.Position(this.getPiecePosition().getX(), this.getPiecePosition().getY() + j);
+            if (!java.util.Objects.isNull(this.getGameObj().getPieceByPosition(newPosition)) && this.getGameObj().getPieceByPosition(newPosition).getGroup() != this.getGroup())
+                att = true;
+            Objects.Move move = new Objects.Move(this, this.getPiecePosition(), newPosition, att);
+            if (this.getGameObj().CheckMove(move))
+                availableMoves.add(move);
+        }
+        return availableMoves;
     }
 }
