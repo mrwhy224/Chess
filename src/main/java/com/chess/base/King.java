83a543a9d8e877fecd4eedcc55b5d9d4a8eead46
Mahdi Roohi <mail@mroohi.ir>
2023-06-13 00:58:57 +0330

    complete AvailableMoves for King

--- src/main/java/com/chess/base/King.java
+++ src/main/java/com/chess/base/King.java
@@ -7,7 +7,18 @@ public class King extends Piece {
         super(Objects.ChessPieces.King, group, gameObj, 0);
     }
     public ArrayList<Objects.Move> AvailableMoves() {
-        // toDo
-        return null;
+        ArrayList<Objects.Move> availableMoves = new ArrayList<>();
+        for (int i=-1; i<2; i++)
+            for (int j=-1; j<2; j++)
+            {
+                boolean att = false;
+                Objects.Position newPosition = new Objects.Position(this.getPiecePosition().getX()+i, this.getPiecePosition().getY()+j);
+                if (!java.util.Objects.isNull(this.getGameObj().getPieceByPosition(newPosition)) && this.getGameObj().getPieceByPosition(newPosition).getGroup() != this.getGroup())
+                    att = true;
+                Objects.Move move = new Objects.Move(this, this.getPiecePosition(), newPosition, att);
+                if(this.getGameObj().CheckMove(move))
+                    availableMoves.add(move);
+            }
+        return availableMoves;
     }
 }
