9c1bafc7d4adefdec6f8320fac97a7fe73a5f660
Mahdi Roohi <mail@mroohi.ir>
2023-06-13 01:24:59 +0330

    complete AvailableMoves for Queen

--- src/main/java/com/chess/base/Queen.java
+++ src/main/java/com/chess/base/Queen.java
@@ -2,12 +2,27 @@ package com.chess.base;
 
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class Queen extends Piece {
     public Queen(PiecesGroup group, Game gameObj) {
         super(Objects.ChessPieces.Queen, group, gameObj, 9);
     }
     public ArrayList<Objects.Move> AvailableMoves() {
-        // toDo
-        return null;
+        ArrayList<Objects.Move> availableMoves = new ArrayList<>();
+
+        for (int i=-this.getPiecePosition().getX(); i<8-this.getPiecePosition().getX(); i++)
+            for (int j=-this.getPiecePosition().getY(); j<8-this.getPiecePosition().getY(); j++)
+                if(i==0 || j==0 || abs(this.getPiecePosition().getX()+i)==abs(this.getPiecePosition().getY()+j))
+                {
+                    boolean att = false;
+                    Objects.Position newPosition = new Objects.Position(this.getPiecePosition().getX()+i, this.getPiecePosition().getY()+j);
+                    if (!java.util.Objects.isNull(this.getGameObj().getPieceByPosition(newPosition)) && this.getGameObj().getPieceByPosition(newPosition).getGroup() != this.getGroup())
+                        att = true;
+                    Objects.Move move = new Objects.Move(this, this.getPiecePosition(), newPosition, att);
+                    if(this.getGameObj().CheckMove(move))
+                        availableMoves.add(move);
+                }
+        return availableMoves;
     }
 }
