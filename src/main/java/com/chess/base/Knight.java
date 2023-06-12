7f4040b3ba14a891c2d0e8dd19e4918d5c3b2f7f
Mahdi Roohi <mail@mroohi.ir>
2023-06-12 23:18:09 +0330

    calcut AvailableMoves for knight

--- src/main/java/com/chess/base/Knight.java
+++ src/main/java/com/chess/base/Knight.java
@@ -6,8 +6,25 @@ public class Knight extends Piece {
     public Knight(PiecesGroup group, Game gameObj) {
         super(Objects.ChessPieces.Knight, group, gameObj, 3);
     }
+
+    private final Objects.Position[] possibleMovement = {
+            new Objects.Position(2,1),
+            new Objects.Position(1,2),
+            new Objects.Position(-1,2),
+            new Objects.Position(-2,1),
+            new Objects.Position(-2,-1),
+            new Objects.Position(-1,-2),
+            new Objects.Position(1,-2),
+            new Objects.Position(2,-1)
+    };
     public ArrayList<Objects.Move> AvailableMoves() {
-        // toDo
-        return null;
+        ArrayList<Objects.Move> availableMoves = new ArrayList<>();
+        for (int i = 0; i< possibleMovement.length; i++)
+        {
+            Objects.Move move = new Objects.Move(this ,this.getPiecePosition(),new Objects.Position(this.getPiecePosition().getX()+ possibleMovement[i].getX(),this.getPiecePosition().getY()+ possibleMovement[i].getY()),false);
+            if(this.getGameObj().CheckMove(move))
+                availableMoves.add(move);
+        }
+        return availableMoves;
     }
 }
