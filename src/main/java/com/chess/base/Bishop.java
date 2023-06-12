9e94bf433f26a0a2f170737c4315ea97f0a1d329
Mahdi Roohi <mail@mroohi.ir>
2023-06-13 01:28:50 +0330

    complete AvailableMoves for Bishop

--- src/main/java/com/chess/base/Bishop.java
+++ src/main/java/com/chess/base/Bishop.java
@@ -2,12 +2,26 @@ package com.chess.base;
 
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class Bishop extends Piece {
     public Bishop(PiecesGroup group, Game gameObj) {
         super(Objects.ChessPieces.Bishop, group, gameObj, 3);
     }
     public ArrayList<Objects.Move> AvailableMoves() {
-        // toDo
-        return null;
+        ArrayList<Objects.Move> availableMoves = new ArrayList<>();
+        for (int i=-this.getPiecePosition().getX(); i<8-this.getPiecePosition().getX(); i++)
+            for (int j=-this.getPiecePosition().getY(); j<8-this.getPiecePosition().getY(); j++)
+                if(abs(this.getPiecePosition().getX()+i)==abs(this.getPiecePosition().getY()+j))
+                {
+                    boolean att = false;
+                    Objects.Position newPosition = new Objects.Position(this.getPiecePosition().getX()+i, this.getPiecePosition().getY()+j);
+                    if (!java.util.Objects.isNull(this.getGameObj().getPieceByPosition(newPosition)) && this.getGameObj().getPieceByPosition(newPosition).getGroup() != this.getGroup())
+                        att = true;
+                    Objects.Move move = new Objects.Move(this, this.getPiecePosition(), newPosition, att);
+                    if(this.getGameObj().CheckMove(move))
+                        availableMoves.add(move);
+                }
+        return availableMoves;
     }
 }
