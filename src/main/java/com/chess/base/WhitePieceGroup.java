b5ad8be8de186c8babfbb4e74c11c665539bda16
Mahdi Roohi <mail@mroohi.ir>
2023-06-14 00:57:32 +0330

    create WhitePieceGroup class

---
+++ src/main/java/com/chess/base/WhitePieceGroup.java
@@ -0,0 +1,7 @@
+package com.chess.base;
+
+public class WhitePieceGroup extends PiecesGroup{
+    public WhitePieceGroup(Objects.PiecesColor color) {
+        super(Objects.PiecesColor.White);
+    }
+}
